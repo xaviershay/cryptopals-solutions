0597d96b268a4234587b97742d0ecff3ff168567
Xavier Shay <contact@xaviershay.com>
2018-03-08 11:55:09 -0800

    Better organization of tests.

---
+++ test/Set1.hs
@@ -0,0 +1,85 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Set1 where
+
+import qualified Data.ByteString.Lazy    as B
+import           Data.Monoid        ((<>))
+import qualified Data.Text.Lazy          as T
+
+import           Test.Tasty
+import           Test.Tasty.HUnit
+
+import Lib
+
+test_Set_1_Challenge_1 = testGroup "Challenge 1"
+  [ testCase "simple hex2bytes" $
+    (Just . B.pack $ [0, 255]) @=? hex2bytes (HexBytes "00ff")
+  , testCase "simple bytes2base64" $
+    Just (Base64 "AAAA") @=? bytes2base64 (B.pack [0])
+  , testCase "invalid hex char"   $ Nothing @=? hex2base64 (HexBytes "ZZ")
+  , testCase "invalid hex length" $ Nothing @=? hex2base64 (HexBytes "F")
+  , testCase "given" $
+    Just (Base64 $ "SSdtIGtpbGxpbmcgeW91ciBicmFpbiB" <>
+                   "saWtlIGEgcG9pc29ub3VzIG11c2hyb29t") @=?
+    hex2base64
+      (HexBytes $ "49276d206b696c6c696e6720796f757220627261696e2" <>
+                  "06c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+  ]
+
+unit_Set_1_Challenge_2 =
+    fromHexString "746865206b696420646f6e277420706c6179" @=?
+      fromHexString "1c0111001f010100061a024b53535009181c" `xorBytes`
+      fromHexString "686974207468652062756c6c277320657965"
+
+unit_Set_1_Challenge_3 = Just "Cooking MC's like a pound of bacon" @=? f
+      (fromHexString $ "1b37373331363f78151b7f2b783431333d7839782" <>
+                       "8372d363c78373e783a393b3736")
+
+  where
+    -- The input cypher is known to xor'ed with a single character. Try decoding
+    -- against all characters, using a simple heuristic to determine which output
+    -- looks most "english like".
+    f :: B.ByteString -> Maybe T.Text
+    f bs =
+      let candidateKeys  = generateSingleCharKeys bs in
+      let candidateTexts = map (xorBytes bs) candidateKeys in
+
+      -- head is safe here because it is operating on the list constructed with a
+      -- constant range above
+      chooseMostLikelyText candidateTexts
+
+unit_Set_1_Challenge_4 = do
+  cipherTexts <- lines <$> readFile "data/4.txt"
+
+  Just "Now that the party is jumping\n" @=? f cipherTexts
+
+  where
+    f :: [String] -> Maybe T.Text
+    f ss =
+      let candidateInputs = map (fromHexString . T.pack) ss in
+      let candidateTexts =
+            [ xorBytes x y
+               | x <- candidateInputs
+               , y <- generateSingleCharKeys x
+            ] in
+
+        chooseMostLikelyText candidateTexts
+
+unit_Set_1_Challenge_5 =
+  (fromHexString $ "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623" <>
+                   "d63343c2a26226324272765272a282b2f20430a652e2c652a" <>
+                   "3124333a653e2b2027630c692b20283165286326302e27282f")
+    @=?
+      xorBytes "ICE"
+        ("Burning 'em, if you ain't quick and nimble\n" <>
+        "I go crazy when I hear a cymbal")
+
+unit_Set_1_Challenge_6 = do
+  cipherText <- concat . lines <$> readFile "data/6.txt"
+
+  Just "XXX\n" @=? f cipherText
+
+  where
+    -- The input cypher is encrypted with repeating key XOR, then base64 encoded.
+    f :: String -> Maybe T.Text
+    f s = Just "TODO"
---
+++ test/Set2.hs
@@ -0,0 +1,6 @@
+module Set2 where
+
+import           Test.Tasty.HUnit
+
+-- Placeholder just to check test loading is working
+unit_Set_2_Challenge_1 = 1 @=? 1
